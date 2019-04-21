ed56bb3cf4d1e22df35dce4d9937f0a212cdfd23
DESKTOP-F0UGBP9\deakl <deak.lala@hotmail.com>
2019-04-21 11:56:00 +0200

    Taking a photo works on both(UWP and Android) platforms.

--- FaceRecognition/FaceRecognition/MainPage.xaml.cs
+++ FaceRecognition/FaceRecognition/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +14,46 @@ namespace FaceRecognition
         public MainPage()
         {
             InitializeComponent();
+
+            CameraButton.Clicked += CameraButton_Clicked;
+        }
+
+        private async void CameraButton_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var status = await CrossPermissions.Current.CheckPermissionStatusAsync(Permission.Camera);
+                if (status != PermissionStatus.Granted)
+                {
+                    if (await CrossPermissions.Current.ShouldShowRequestPermissionRationaleAsync(Permission.Camera))
+                    {
+                        await DisplayAlert("Camera required!", "This feature requires your phones camera.", "OK");
+                    }
+
+                    var results = await CrossPermissions.Current.RequestPermissionsAsync(Permission.Camera);
+                    status = results[Permission.Camera];
+                }
+
+                if (status == PermissionStatus.Granted)
+                {
+                    //var photo = await Plugin.Media.CrossMedia.Current.PickPhotoAsync();
+                    var photo = await Plugin.Media.CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions() { });
+                    if (photo != null)
+                    {
+                        PhotoImage.Source = ImageSource.FromStream(() => { return photo.GetStream(); });
+                    }
+                }
+                else if (status != PermissionStatus.Unknown)
+                {
+                    await DisplayAlert("Camera Denied!", "You have to allow the use of the camera.", "OK");
+
+                }
+            }
+
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error!", ex.Message, "OK");
+            }
         }
     }
 }
