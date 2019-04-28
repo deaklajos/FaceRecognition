f7716e3f01915a100fd577a897f7c47d67c595b8
DESKTOP-F0UGBP9\deakl <deak.lala@hotmail.com>
2019-04-28 19:27:07 +0200

    Moved camera access feature into ImageProvider Service.

--- FaceRecognition/FaceRecognition/MainPage.xaml.cs
+++ FaceRecognition/FaceRecognition/MainPage.xaml.cs
@@ -1,10 +1,5 @@
-﻿using Plugin.Permissions;
-using Plugin.Permissions.Abstractions;
+﻿using FaceRecognition.Services;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace FaceRecognition
@@ -20,40 +15,8 @@ namespace FaceRecognition
 
         private async void CameraButton_Clicked(object sender, EventArgs e)
         {
-            try
-            {
-                var status = await CrossPermissions.Current.CheckPermissionStatusAsync(Permission.Camera);
-                if (status != PermissionStatus.Granted)
-                {
-                    if (await CrossPermissions.Current.ShouldShowRequestPermissionRationaleAsync(Permission.Camera))
-                    {
-                        await DisplayAlert("Camera required!", "This feature requires your phones camera.", "OK");
-                    }
-
-                    var results = await CrossPermissions.Current.RequestPermissionsAsync(Permission.Camera);
-                    status = results[Permission.Camera];
-                }
-
-                if (status == PermissionStatus.Granted)
-                {
-                    //var photo = await Plugin.Media.CrossMedia.Current.PickPhotoAsync();
-                    var photo = await Plugin.Media.CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions() { });
-                    if (photo != null)
-                    {
-                        PhotoImage.Source = ImageSource.FromStream(() => { return photo.GetStream(); });
-                    }
-                }
-                else if (status != PermissionStatus.Unknown)
-                {
-                    await DisplayAlert("Camera Denied!", "You have to allow the use of the camera.", "OK");
-
-                }
-            }
-
-            catch (Exception ex)
-            {
-                await DisplayAlert("Error!", ex.Message, "OK");
-            }
+            var provider = new ImageProvider();
+            PhotoImage.Source = await provider.PickImageAsync(this);
         }
     }
 }
